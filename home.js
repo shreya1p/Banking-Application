78e99c2219bc053bfa4f53ab276a286aeeab2050
Shreya Patel <101183950+shreya1p@users.noreply.github.com>
2022-09-07 16:42:24 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,11 @@
-const Home = () => {
+function Home(){
   return (
-    <div className="container my-3">
-      <Card
-        header="BadBank Landing Page"
-        title="Welcome to the Bad Bank"
-        text="You can use this bank"
-        body={
-          <img
-            src="./resources/bank.png"
-            className="img-fluid"
-            alt="Bank Logo"
-          />
-        }
-      />
-    </div>
-  );
-};
+    <Card
+      txtcolor="black"
+      header="BadBank Landing Module"
+      title="Welcome to the bank"
+      text="You can trust us with your money"
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
